2fef882e24072f772a3b807d198195fd42fb39b7
andre-mf <andremendesfelix@gmail.com>
2024-11-12 20:48:22 -0300

    Criar os repositories das entities

---
+++ src/main/java/com/andre/springsecurity/repository/TweetRepository.java
@@ -0,0 +1,9 @@
+package com.andre.springsecurity.repository;
+
+import com.andre.springsecurity.entities.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+}
