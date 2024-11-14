0dc91108b1de3fe2dea7af2b34d4a03a96dcd953
andre-mf <andremendesfelix@gmail.com>
2024-11-13 23:32:41 -0300

    Criar a funcionalidade de deletar tweet

--- src/main/java/com/andre/springsecurity/controller/TweetController.java
+++ src/main/java/com/andre/springsecurity/controller/TweetController.java
@@ -1,14 +1,15 @@
 package com.andre.springsecurity.controller;
 
 import com.andre.springsecurity.controller.dto.CreateTweetDto;
+import com.andre.springsecurity.entities.Role;
 import com.andre.springsecurity.entities.Tweet;
 import com.andre.springsecurity.repository.TweetRepository;
 import com.andre.springsecurity.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -33,6 +34,27 @@ public class TweetController {
         tweet.setContent(dto.content());
 
         tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
+                                            JwtAuthenticationToken token) {
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get().getRoles()
+                .stream()
+                .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+            tweetRepository.delete(tweet);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
 
         return ResponseEntity.ok().build();
     }
