91fd0574ed1b797cd2e2b5cc158f89974f2c4f0f
andre-mf <andremendesfelix@gmail.com>
2024-11-13 23:03:05 -0300

    Criar a funcionalidade de listar todos os usuários

--- src/main/java/com/andre/springsecurity/controller/TokenController.java
+++ src/main/java/com/andre/springsecurity/controller/TokenController.java
@@ -2,6 +2,7 @@ package com.andre.springsecurity.controller;
 
 import com.andre.springsecurity.controller.dto.LoginRequest;
 import com.andre.springsecurity.controller.dto.LoginResponse;
+import com.andre.springsecurity.entities.Role;
 import com.andre.springsecurity.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
@@ -39,11 +41,17 @@ public class TokenController {
         var now = Instant.now();
         var expiresIn = 300L;
 
+        var scopes = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+
         var claims = JwtClaimsSet.builder()
                 .issuer("mybackend")
                 .subject(user.get().getUserId().toString())
                 .issuedAt(now)
                 .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
                 .build();
 
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
